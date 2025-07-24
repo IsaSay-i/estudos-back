bc69e3dee51a99b1ccbc06d530740016ccf707d1
Isabelle Sayuri Isa <isabelle.say.isa@gmail.com>
2025-07-24 14:00:06 -0300

    Enfim entendi um pouco de classes em python :)

---
+++ Python/Livro/classes/cachorro.py
@@ -0,0 +1,30 @@
+class Cachorro:
+    info = []
+    
+    def __init__(self, nome, idade, raca):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+        
+        Cachorro.info.append({"nome": self.nome, "idade": self.idade, "raca": self.raca})
+        
+    @classmethod
+    def exibir_info(cls):
+        print("Informações:")
+        for item in cls.info:
+            print(f"Nome: {item["nome"]}\nIdade: {item["idade"]}\nRaça: {item["raca"]}\n")
+        
+    @classmethod
+    def latir(cls):
+        for item in cls.info:
+            print(f"{item["nome"]}: AuAu!")
+
+for i in range(2):
+    nome = input(f"Nome do cachorro {i+1}: ")
+    idade = input(f"Idade do cachorro {i+1}: ")
+    raca = input(f"Raça do cachorro {i+1}: ")
+
+    Cachorro(nome, idade, raca)
+    
+Cachorro.exibir_info()
+Cachorro.latir()
--- Python/jogo_da_velha.py
+++ Python/jogo_da_velha.py
@@ -13,27 +13,3 @@ def mostrar_tabuleiro():
             print("|", casa ,end="|")
     print()
 
-class Jogador:
-    def __init__(self):
-        self.numero = 0
-        self.simbolo = ""
-        self.simbolos_validos = ["x", "o"]
-        self.jogadores = []
-
-        escolha = ""
-        while escolha not in self.simbolos_validos:
-            escolha = input("Jogador 1\nEscolha o seu símbolo: (x|o)\n: ").lower()
-            if escolha not in self.simbolos_validos:
-                print("\nInválido! Selecione 'x' ou 'o'\n")
-            else:
-                self.jogadores.append({"numero": 1, "simbolo": escolha})
-                self.jogadores.append({
-                    "numero": 2,
-                    "simbolo": "x" if self.jogadores[0]["simbolo"] == "o" else "o"
-                })
-
-        for i in range(2):
-            print(f"Jogador {self.jogadores[i][self.numero]}: {self.jogadores[i][self.simbolo]}")
-
-            
-Jogador()
