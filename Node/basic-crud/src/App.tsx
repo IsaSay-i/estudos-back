e58c3b685bcb11137ccd98de65f6b9e24ec5bbae
Isabelle Sayuri Isa <isabelle.say.isa@gmail.com>
2025-08-08 02:48:18 -0300

    style: signup -> signUp

--- Node/basic-crud/src/App.tsx
+++ Node/basic-crud/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signUp" element={<SignUp />} />
 
           <Route path="/dashboard" element={<Dashboard />}>
             <Route index element={<Home />} />
